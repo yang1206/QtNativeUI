a57d82d1055a64e17e7b9ffc2ec4a8917d7757b9
yang1206 <yang1206@outlook.com.tr>
2025-03-18 21:06:50 +0800

    feat:  checkbox

--- include/QtNativeUI/NCheckBox.h
+++ include/QtNativeUI/NCheckBox.h
@@ -2,12 +2,24 @@
 #define QTNATIVEUI_NCHECKBOX_H
 
 #include <QCheckBox>
+#include "NIconEnums.h"
 #include "stdafx.h"
 
 class NCheckBoxPrivate;
 class QTNATIVEUI_EXPORT NCheckBox : public QCheckBox {
     Q_OBJECT
     Q_Q_CREATE(NCheckBox)
+    Q_PROPERTY_CREATE_Q_H(int, BorderRadius)
+    Q_PROPERTY_CREATE_Q_H(QColor, LightDefaultColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, DarkDefaultColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, LightHoverColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, DarkHoverColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, LightPressColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, DarkPressColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, LightTextColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, DarkTextColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, LightBorderColor)
+    Q_PROPERTY_CREATE_Q_H(QColor, DarkBorderColor)
 
   public:
     explicit NCheckBox(QWidget* parent = nullptr);
@@ -16,9 +28,18 @@ class QTNATIVEUI_EXPORT NCheckBox : public QCheckBox {
 
   protected:
     void paintEvent(QPaintEvent* event) override;
+    void enterEvent(QEnterEvent* event) override;
+    void leaveEvent(QEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
+    void changeEvent(QEvent* event) override;
+
+    void drawCheckBox(QPainter* painter);
+    void drawText(QPainter* painter);
 
   private:
     void init();
+    void updateAccentColors();
 };
 
-#endif // QTNATIVEUI_NCHECKBOX_H 
+#endif // QTNATIVEUI_NCHECKBOX_H
