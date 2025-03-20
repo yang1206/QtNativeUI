9e2cb346ab8df3568a3b71a06d006df81b59f185
yang1206 <yang1206@outlook.com.tr>
2025-03-21 07:42:28 +0800

    feat(Icon):  Custom QIconEngine supports theme-aware icon rendering

--- include/QtNativeUI/NIcon.h
+++ include/QtNativeUI/NIcon.h
@@ -2,10 +2,30 @@
 #define QTNATIVEUI_NICON_H
 
 #include <QIcon>
+#include <QIconEngine>
 #include <QtCore/qmap.h>
 
 #include "NIconEnums.h"
 #include "singleton.h"
+
+class NFluentIconEngine : public QIconEngine {
+  public:
+    NFluentIconEngine(bool isRegular, uint32_t iconCode, int size, const QColor& customColor = QColor());
+    ~NFluentIconEngine() override;
+
+    void         paint(QPainter* painter, const QRect& rect, QIcon::Mode mode, QIcon::State state) override;
+    QIconEngine* clone() const override;
+    QSize        actualSize(const QSize& size, QIcon::Mode mode, QIcon::State state) override;
+    QPixmap      pixmap(const QSize& size, QIcon::Mode mode, QIcon::State state) override;
+
+  private:
+    bool     isRegular;
+    uint32_t iconCode;
+    int      iconSize;
+    QColor   customColor;
+
+    QColor getEffectiveColor(QIcon::Mode mode) const;
+};
 
 class QTNATIVEUI_EXPORT NIcon {
     Q_SINGLETON_CREATE_H(NIcon)
@@ -23,7 +43,6 @@ class QTNATIVEUI_EXPORT NIcon {
     QMap<QString, FontConfig> fontConfigs;
     void                      initFontConfigs();
 
-    // 通用的图标生成方法
     QIcon createIcon(const QString& fontFamily,
                      uint32_t       unicode,
                      int            size   = 24,
@@ -31,10 +50,11 @@ class QTNATIVEUI_EXPORT NIcon {
                      int            width  = 0,
                      int            height = 0);
 
+    QIcon createThemeAwareIcon(bool isRegular, uint32_t unicode, int size = 24, const QColor& customColor = QColor());
+
     QColor getThemeBasedColor() const;
 
   public:
-    // Regular 图标
     QIcon fromRegular(NRegularIconType::Icon icon);
     QIcon fromRegular(NRegularIconType::Icon icon, const QColor& color);
     QIcon fromRegular(NRegularIconType::Icon icon, int size);
@@ -42,13 +62,16 @@ class QTNATIVEUI_EXPORT NIcon {
     QIcon fromRegular(NRegularIconType::Icon icon, int size, int width, int height);
     QIcon fromRegular(NRegularIconType::Icon icon, int size, int width, int height, const QColor& color);
 
-    // Filled 图标
     QIcon fromFilled(NFilledIconType::Icon icon);
     QIcon fromFilled(NFilledIconType::Icon icon, const QColor& color);
     QIcon fromFilled(NFilledIconType::Icon icon, int size);
     QIcon fromFilled(NFilledIconType::Icon icon, int size, const QColor& color);
     QIcon fromFilled(NFilledIconType::Icon icon, int size, int width, int height);
     QIcon fromFilled(NFilledIconType::Icon icon, int size, int width, int height, const QColor& color);
+
+    QString getRegularFontFamily() const { return fontConfigs["Regular"].fontFamily; }
+    QString getFilledFontFamily() const { return fontConfigs["Filled"].fontFamily; }
+    QString getIconText(uint32_t unicode);
 };
 
 #define nIcon NIcon::getInstance()
