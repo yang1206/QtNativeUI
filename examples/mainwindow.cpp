9de62c8aa8c989a4bad678424902bedd6504d7a4
yang1206 <yang15188280673@outlook.com>
2025-06-24 18:08:41 +0800

    fix: 修复字体导致msvc debug模式抛出异常

--- examples/mainwindow.cpp
+++ examples/mainwindow.cpp
@@ -13,13 +13,13 @@ MainWindow::MainWindow(QWidget* parent) : NMainWindow(parent) {
     setCentralWidget(m_navigationView);
     auto button = new ButtonExample(this);
     button->setMainWindow(this);
-    // m_navigationView->addPageNode("Button", button, NRegularIconType::Button16Regular);
-    // m_navigationView->addPageNode("Controls", new ControlsExample(this), NRegularIconType::Accessibility16Regular);
-    // m_navigationView->addPageNode("Menus", new MenuExample(this), NRegularIconType::MoreHorizontal16Regular);
-    // m_navigationView->addPageNode("Dialogs", new DialogsExample(this), NRegularIconType::Flag16Regular);
-    // m_navigationView->addPageNode("Navigation", new NavigationExample(this), NRegularIconType::Router20Regular);
-    // m_navigationView->addPageNode("Colors", new ColorsExample(this), NRegularIconType::Color16Regular);
-    // m_navigationView->addPageNode("Icons", new IconExample(this), NRegularIconType::Icons20Regular);
+    m_navigationView->addPageNode("Button", button, NRegularIconType::Button16Regular);
+    m_navigationView->addPageNode("Controls", new ControlsExample(this), NRegularIconType::Accessibility16Regular);
+    m_navigationView->addPageNode("Menus", new MenuExample(this), NRegularIconType::MoreHorizontal16Regular);
+    m_navigationView->addPageNode("Dialogs", new DialogsExample(this), NRegularIconType::Flag16Regular);
+    m_navigationView->addPageNode("Navigation", new NavigationExample(this), NRegularIconType::Router20Regular);
+    m_navigationView->addPageNode("Colors", new ColorsExample(this), NRegularIconType::Color16Regular);
+    m_navigationView->addPageNode("Icons", new IconExample(this), NRegularIconType::Icons20Regular);
 
     // 设置窗口属性
     setWindowTitle("QtNativeUI by Yang1206");
